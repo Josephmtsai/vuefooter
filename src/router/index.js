f709cc7b21d54eff80feeba834c68035bb318250
JosephTsai <mmx362003@gmail.com>
2018-01-23 22:38:30 +0800

    add package

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Footer from '@/components/Footer';
+import Vuex from 'vuex';
 
+// load vuex i18n module
+import vuexI18n from 'vuex-i18n';
+const store = new Vuex.Store();
 Vue.use(Router);
+Vue.use(vuexI18n.plugin, store);
+Vue.i18n.add('en', translationsEn);
+Vue.i18n.add('de', translationsDe);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Footer',
+      component: Footer,
     },
   ],
 });
