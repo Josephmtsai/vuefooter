cc4b1e346db035984e1967a2fe894070bbbe4ed2
Joseph <mmx362003@gmail.com>
2018-02-06 15:08:59 +0800

    change style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,35 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2><h1>{{ 'test1' | translate }}</h1></h2>
-    <select v-model="SelectedLanguage">
-        <option value="en">EN</option>
-        <option value="id">ID</option>
-    </select>
-    
+  <div >
+    <b-container class="bv-example-row">
+      <b-row >
+        <h1  class="hello" >{{ msg }}</h1>
+        <p/>
+        <h2><h1>{{ 'test1' | translate }}</h1></h2>
+      </b-row>
+      <b-row class="justify-content-md-center">
+        <b-col cols="12" md="auto">
+          <select v-model="SelectedLanguage">
+            <option value="en">EN</option>
+            <option value="id">ID</option>
+          </select>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col sm><b-btn v-b-popover.hover="'I am popover content!'" title="Popover Title">Hover Me</b-btn></b-col>
+        <b-col sm>col-sm</b-col>
+        <b-col sm>col-sm</b-col>
+    </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 export default {
-  name: 'vuefooter',
+  name: 'vuetest',
   data() {
     return {
-      msg: 'This is the contents',
+      msg: 'This is the Main contents',
     };
   },
   async created () {
@@ -58,7 +71,7 @@ a {
 }
 
 .hello {
-  font-size: 12px;
+  font-size: 18px;
   background: #363636;
   color: #818181;
   }
--- src/main.js
+++ src/main.js
@@ -2,17 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuex from 'vuex';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 // load vuex i18n module
 import App from './App';
 import router from './router';
 import store from './store';
+
+
 Vue.use(Vuex);
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#vuefooter',
+  el: '#vuetest',
   router,
   components: { App },
   store,
